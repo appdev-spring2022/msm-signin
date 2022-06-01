4b2eb935855eacb73d8d5ffc96cf6e12f9242eab
utkarshgarg92 <ugarg@chicagobooth.edu>
2022-06-01 00:32:12 +0000

    new_version_3

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,37 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def director
+    my_director_id = self.director_id
+
+    matching_directors = Director.where({ :id => my_director_id })
+
+    the_director = matching_directors.at(0)
+
+    return the_director
+  end
+
+  def characters
+    my_id = self.id
+
+    matching_characters = Character.where({ :movie_id => my_id })
+
+    return matching_characters
+  end
+
+  def cast
+    array_of_actor_ids = Array.new
+
+    my_characters = self.characters
+
+    my_characters.each do |a_character|
+      the_actor = a_character.actor
+
+      array_of_actor_ids.push(the_actor.id)
+    end
+
+    matching_actors = Actor.where({ :id => array_of_actor_ids })
+
+    return matching_actors
+  end
 end
